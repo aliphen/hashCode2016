e264421b2bc48ee17f8e00cd1f0dda6b43a5611e
Raphaël Vandon <r.vandon@criteo.com>
2016-02-10 16:19:20 +0100

    initial commit

---
+++ Hashcode.Qualif/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hashcode.Qualif
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
