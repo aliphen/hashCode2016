31d7eb3887708997e8285b6d59d1a6513051c3ae
Raphaël Vandon <r.vandon@criteo.com>
2016-02-11 19:29:29 +0100

    add drone class

---
+++ Hashcode.Qualif/Drone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Hashcode.Qualif
+{
+    public class Drone
+    {
+        public int X;
+        public int Y;
+        public int turn = 0;
+
+        public Drone(Input input)
+        {
+            X = input.WareHouses[0].X;
+            Y = input.WareHouses[0].Y;
+        }
+    }
+}
+
--- Hashcode.Qualif/Solver.cs
+++ Hashcode.Qualif/Solver.cs
@@ -15,8 +15,22 @@ namespace Hashcode.Qualif
 
             Helper.Assert(() => true, "optional message");
 
+            for(int o = 0; o < input.Orders.Length; o++)
+            {
+                var order = input.Orders[o];
+                for (int i = 0; i < order.ItemsWanted.Length; i++)
+                {
+                    //chooseDrone
+                    var itemType = order.ItemsWanted[i];
+                    solution.Builder.AppendLine("{0} L {1} {2} {3}", droneId, warehouseId, itemType, 1);
+                    nbCommands++;
+                    solution.Builder.AppendLine("{0} D {1} {2} {3}", droneId, o, itemType, 1);
+                    nbCommands++;
+                    //update drone status
+                }
+            }
 
-
+            solution.Builder.Insert(0, nbCommands);
             return solution;
         }
 	}
