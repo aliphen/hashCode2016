fd4fcadcaf0fb98cb5bac09bb5cfd7a5fceec965
Raphael Vandon <vandonr@gmail.com>
2016-03-21 01:39:16 +0100

    wrote parser

--- Hashcode.Finale/Input.cs
+++ Hashcode.Finale/Input.cs
@@ -6,12 +6,62 @@ using System.Security.Policy;
 
 namespace Hashcode.Qualif
 {
+    public class Coords
+    {
+        public int Lat;
+        public int Lon;
+    }
+
+    public class Satellite
+    {
+        public readonly Coords Pos;
+        public readonly int Speed;
+        public readonly int RotSpeed;
+        public readonly int MaxRot;
+
+        public Satellite(int lat, int lon, int speed, int rotSpeed, int maxRot)
+        {
+            Pos = new Coords {Lat = lat, Lon = lon};
+            Speed = speed;
+            RotSpeed = rotSpeed;
+            MaxRot = maxRot;
+        }
+    }
+
+    public class TimeRange
+    {
+        public readonly int Start;
+        public readonly int End;
+
+        public TimeRange(int start, int end)
+        {
+            Start = start;
+            End = end;
+        }
+    }
+
+    public class PicCollection
+    {
+        public readonly int Value;
+        public readonly List<Coords> Locations;
+        public readonly List<TimeRange> TimeRanges;
+
+        public PicCollection(int value)
+        {
+            Value = value;
+            Locations = new List<Coords>();
+            TimeRanges = new List<TimeRange>();
+        }
+    }
+
     /// <summary>
     /// contains an object representation of the input
     /// </summary>
 	public class Input
-	{
-
-	}
+    {
+        public int NbTurns;
+        public readonly List<Satellite> Satellites = new List<Satellite>();
+        public readonly List<PicCollection> Collections = new List<PicCollection>();
+    }
 }
 
--- Hashcode.Finale/Parser.cs
+++ Hashcode.Finale/Parser.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -10,12 +9,59 @@ namespace Hashcode.Qualif
         public static Input Parse(string fileName)
         {
             var input = new Input();
-            using(var reader = new StreamReader(fileName))
+            using (var reader = new StreamReader(fileName))
             {
+                input.NbTurns = ReadInt(reader);
 
+                var nbSat = ReadInt(reader);
+                for (int i = 0; i < nbSat; i++)
+                {
+                    var line = ReadMulti(reader);
+                    var sat = new Satellite(
+                        lat: line[0],
+                        lon: line[1],
+                        speed: line[2],
+                        rotSpeed: line[3],
+                        maxRot: line[4]);
+                    input.Satellites.Add(sat);
+                }
+
+                var nbCollec = ReadInt(reader);
+                for (int i = 0; i < nbCollec; i++)
+                {
+                    var line = ReadMulti(reader);
+                    var collec = new PicCollection(value: line[0]);
+                    var nbLoc = line[1];
+                    for (int j = 0; j < nbLoc; j++)
+                    {
+                        var loc = ReadMulti(reader);
+                        collec.Locations.Add(new Coords {Lat = loc[0], Lon = loc[1]});
+                    }
+                    var nbRanges = line[2];
+                    for (int j = 0; j < nbRanges; j++)
+                    {
+                        var time = ReadMulti(reader);
+                        collec.TimeRanges.Add(new TimeRange(time[0], time[1]));
+                    }
+                    input.Collections.Add(collec);
+                }
             }
             return input;
         }
+
+        #region helper methods
+
+        private static int ReadInt(StreamReader reader)
+        {
+            return Int32.Parse(reader.ReadLine());
+        }
+
+        private static int[] ReadMulti(StreamReader reader)
+        {
+            return reader.ReadLine().Split(' ').Select(Int32.Parse).ToArray();
+        }
+
+        #endregion
     }
 }
 
